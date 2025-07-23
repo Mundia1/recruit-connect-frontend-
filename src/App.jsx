e3d691d35904d4342eeb3ea50d7ee8d7e62f44d5
Priscillah Njai <priscillahnjaii@gmail.com>
2025-07-23 15:39:18 +0300

    Add feedback form for logged in users only

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import JobSeekerDashboard from './pages/JobSeekerDashboard';
 import ProtectedRoute from './components/auth/ProtectedRoute';
+import Feedback from "./pages/Feedback";
+
 
 function App() {
   return (
@@ -28,13 +30,13 @@ function App() {
       <Route path="/admin/applicants" element={<AdminDashboard />} />
       <Route path="/admin/settings" element={<AdminDashboard />} />
       <Route path="/admin/help" element={<AdminDashboard />} />
-      <Route path="/admin/feedback" element={<AdminDashboard />} />
 
 
       {/* Protected Routes */}
       <Route element={<ProtectedRoute />}>
         <Route path="/dashboard" element={<JobSeekerDashboard />} />
         <Route path="/profile" element={<JobSeekerDashboard />} />
+        <Route path="/feedback" element={<Feedback />} />
       </Route>
     </Routes>
   );
